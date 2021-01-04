a70f70a1ed9e62800da81a7b8e7d5a519d1ab84c
Reba1Faith26 <67510270+Reba1Faith26@users.noreply.github.com>
2021-01-05 00:34:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function menuButton(){
+  $(document).ready(function(){
+    $(".menu-button").click(function(){
+      $(".navigation-list").slideToggle(350);
+    })
+  })
+}
+
+var i = 0;
+var welcome = " Welcome To Forex Guru trading institute"; 
+/* The text */
+var speed = 40; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < welcome.length) {
+    document.getElementById("type-writer-caption").innerHTML += welcome.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function midCaptionAnimation(){
+  $(".mid-caption").animate({
+    marginRight : "0px"
+  },2200)
+}
+menuButton();
+typeWriter();
